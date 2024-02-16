7930cdb900308837ad8e31b0e7a2acbd570fc30d
ShaolinmatadorPrateado <leonardogmcorrea@gmail.com>
2024-02-15 22:23:49 -0300

    15/02

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using VSComMariaDB.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
+
 builder.Services.AddDbContext<_DbContext>();
 
 
@@ -11,6 +13,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(X=>
+X.AllowAnyHeader()
+.AllowAnyMethod().AllowAnyOrigin()
+);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
